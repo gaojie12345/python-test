17030fdd65c7e0b5dd06b8cf3554ef0b51a58393
Olsen <gaojie2844@wjinfo.cn>
2024-01-25 16:47:15 +0800

    feat:redis相关

--- Day16-redis.py
+++ Day16-redis.py
@@ -3,7 +3,7 @@
 
 import redis
 
-client = redis.Redis(host='10.0.220.30', port=6379, password='wj@2022')
+client = redis.Redis(host='xxxxxx', port=6379, password='xxxxxxx')
 
 client.set("qifei", 'amin')
 client.hset("qifei1", 'amin', '111')
